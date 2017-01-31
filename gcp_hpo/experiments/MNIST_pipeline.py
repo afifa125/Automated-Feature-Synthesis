e82f2e01d1c06fc351080454527f7c82b8bfb939
Alec <alecand@mit.edu>
2017-01-31 16:55:20 -0500

    MNIST pipeline finished and incorporated into SmartSearch with a RandomForest classifier

--- gcp_hpo/experiments/MNIST_pipeline.py
+++ gcp_hpo/experiments/MNIST_pipeline.py
@@ -5,66 +5,67 @@ Running MNIST pipeline entirely online, rather than from csv files like currentl
 from gcp_hpo.smart_search import SmartSearch
 import numpy as np
 import math
-
-def branin(x, y):
-	"""
-	The opposite of the Branin function.
-	"""
-	result = np.square(y - (5.1/(4*np.square(math.pi)))*np.square(x) + \
-		(5/math.pi)*x - 6) + 10*(1-(1./(8*math.pi)))*np.cos(x) + 10
-	return [-result]
-
-
-def mnist_pipeline_tmp():
-	"""
-	Steps:
-		1. Import MNIST data (from Kaggle to be consistent with Sebastien)
-		2. Blur the training images
-		3. Reduce image matrices by principal component analysis
-		4. classify using polynomial SVM
-	"""
-
-	# Import MNIST data
-
-
-
-# def scoring_function(p_dict):
-# 	x,y = p_dict['x'], p_dict['y']
-# 	x = x -5.
-# 	y= y
-# 	return branin(x,y)
+from scipy.ndimage.filters import gaussian_filter 
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from cv2 import GaussianBlur
+from sklearn.ensemble import RandomForestClassifier
 
 def import_data():
-	train_data = np.loadtxt('/Users/aandersonlaptop/Desktop/MNIST_train.csv',skiprows=1,delimiter=',')
-    train_x = train_data[:,1:]
-    train_y = train_data[:,0]
+	# TODO replace this with new test_train_split thing probably
+		# and put in a random sampling option so can test with smaller data sets
+
+    data = np.loadtxt('/Users/aandersonlaptop/Desktop/MNIST_train.csv',skiprows=1,delimiter=',')
+    train_x = data[:40000,1:]
+    train_y = data[:40000,0]
     # print 'train Y',train_y
     # print 'train X',train_x
-    test_data = np.loadtxt('/Users/aandersonlaptop/Desktop/MNIST_test.csv',skiprows=1,delimiter=',') # way slower than pandas
-    test_x = test_data[:,1:]
-    test_y = test_data[:,0]
-    # print 'test Y',test_y
+    test_x = data[40000:,1:]
+    test_y = data[40000:,0]
+    
     # print 'test X',test_x
     return train_x,train_y,test_x,test_y
+    
+    
+X_train,y_train,X_test,y_test = import_data()
 
-# TODO set X_train and y_train here (subsets, first 5000, etc)
-# X_train =
-# y_train =
-# X_test =
-# y_test = 
+def gaussian_blur(X,kernel_size,stddev):
+    output = np.zeros(X.shape)
+    for i in range(X.shape[0]):
+        output[i,]= np.reshape(GaussianBlur(X[i,],(kernel_size,kernel_size),sigmaX=stddev,sigmaY=stddev),\
+                         (X.shape[1]))
+    return output
+
+def do_pca(X,num_components,fit,fitted_model=None):
+    if fit:
+        pca = PCA(n_components = num_components)
+        pca = pca.fit(X)
+        return pca,pca.transform(X)
+    else:
+        return fitted_model.transform(X)
+
+def random_forest(X,y,num_estimators):
+    clf = RandomForestClassifier(n_estimators=num_estimators)
+    clf.fit(X,y)
+    print 'accuracy on training set',clf.score(X,y)
+    return clf
+
 
 def scoring_function(p_dict):
 	# parameters: blur_ksize,blur_sigma,pca_dim/10,degree,log10(gamma*1000)
 
 	# Train model
-	blurred_X_train = gaussian_blur(X_train,stddev=p_dict['blur_sigma'],k_size=p_dict['blur_ksize'])
-	pca_X_train = do_pca(blurred_X_train,num_components=p_dict['pca_dim'])
-	model = do_svm(pca_X_train,y_train,degree=p_dict['degree_poly'],gamma_coeff=p_dict['gamma'])
+	blurred_X_train = gaussian_blur(X_train,kernel_size = p_dict['blur_ksize'],\
+		stddev = p_dict['blur_sigma'])
+	pca_model,pca_X_train = do_pca(blurred_X_train,num_components=p_dict['pca_dim'],fit = True)
+	model = random_forest(pca_X_train,y_train,num_estimators=20)
 
 	# Test model
-	blurred_X_test = gaussian_blur(X_test,stddev=p_dict['blur_sigma'],k_size=p_dict['blur_ksize'])
-	pca_X_test = do_pca(blurred_X_test,num_components=p_dict['pca_dim'])
-	return model.score(pca_X_test,y_test)
+	blurred_X_test = gaussian_blur(X_test,kernel_size = p_dict['blur_ksize'],\
+		stddev = p_dict['blur_sigma'])
+	pca_X_test = do_pca(blurred_X_test,num_components=p_dict['pca_dim'],fit=False,\
+		fitted_model=pca_model)
+	return [model.score(pca_X_test,y_test)]
 
 
 
@@ -72,11 +73,12 @@ def main():
 	# TODO maybe change the arguments to SmartSearch to be the same as for MNIST in the experiments folder
 
 	### Set parameters ###
-	parameters = { 'blur_sigma' : ['int',[0,1]],
-	               'blur_ksize' : ['int',[1,1]],
+	parameters = { 'blur_ksize' : ['cat',[3,5]],
+				   'blur_sigma' : ['int',[0,1]],
 	               'pca_dim' : ['int',[50,300]],
-	               'degree_poly' : ['int',[1,4]],
-	               'gamma' : ['int',[int(10**(-3)),int(10**(1))]] }
+	               'num_trees': ['int',[5,30]]}
+	               # 'degree_poly' : ['int',[1,4]],
+	               # 'gamma' : ['int',[int(10**(-3)),int(10**(1))]] }
 	nugget = 1.e-10
 	n_clusters = 1
 	cluster_evol ='constant'
