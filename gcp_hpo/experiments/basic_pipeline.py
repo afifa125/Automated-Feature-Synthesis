f49f61a6cf4c5a2dd4576c89bbe5a28c8e17cbfa
Alec <alecand@mit.edu>
2017-01-31 16:09:36 -0500

    basic_pipeline cleaned

--- gcp_hpo/experiments/basic_pipeline.py
+++ gcp_hpo/experiments/basic_pipeline.py
@@ -47,10 +47,6 @@ def load_data():
     return train_test_split(X,y,test_size=0.33) 
 
 X_train,X_test,y_train,y_test = load_data()
-# print 'X_train',X_train.shape
-# print 'y_train',y_train.shape
-# print 'X_test',X_test.shape
-# print 'y_test',y_test.shape
 
 def do_pca(X,num_components,fit,fitted_model=None):
     if fit:
@@ -66,35 +62,50 @@ def random_forest(X,y,num_estimators):
     print 'accuracy on training set',clf.score(X,y)
     return clf
 
-def blb_main(X,y,calculate_statistic,n,b,s,r,p_dict,pca_model,forest_model):
-    subsample_estimate_sum = 0.0
-    pval_array = np.ones(b)/float(b)
-    # Randomly sample subset of indices
-    idx = np.random.randint(low=0,high=n,size=(s,b))
-    for j in range(s):
-        # Approximate the measure
-        monte_carlo_estimate_sum= 0.0
-        multinomial_sample = np.random.multinomial(n,pvals=pval_array,size=r)
-        for k in range(r):
-            monte_carlo_estimate_sum += calculate_statistic(X,y,multinomial_sample[k,]\
-                                                            ,idx[j,],p_dict,pca_model,forest_model)
-        subsample_estimate_sum += monte_carlo_estimate_sum/float(r)
-    
-    return subsample_estimate_sum/float(s)
+def blb_main(X,y,calculate_statistic,p_dict,pca_model,forest_model):
+	#### Define variables ######
+	n = X_test.shape[0] # Size of data
+	b = int(n**(0.7)) # subset size
+	s = 1 # Number of sampled subsets
+	r = 20 # Number of Monte Carlo iterations
+
+	#### Algorithm ####
+	subsample_estimate_sum = 0.0
+	pval_array = np.ones(b)/float(b)
+	# Randomly sample subset of indices
+	idx = np.random.randint(low=0,high=n,size=(s,b))
+	for j in range(s):
+	    # Approximate the measure
+	    monte_carlo_estimate_sum= 0.0
+	    multinomial_sample = np.random.multinomial(n,pvals=pval_array,size=r)
+	    for k in range(r):
+	        monte_carlo_estimate_sum += calculate_statistic(X,y,multinomial_sample[k,]\
+	                                             ,idx[j,],p_dict,pca_model,forest_model)
+	    subsample_estimate_sum += monte_carlo_estimate_sum/float(r)
+
+	return subsample_estimate_sum/float(s)
 
 
 def calculate_accuracy(X,y,sample,indices,p_dict,pca_model,model):
-	new_X = X[indices]
-	new_y = y[indices]
+	sampled_X = X[indices]
+	sampled_Y = y[indices]
 
 	# Test model
-	pca_X_test = do_pca(new_X,num_components=p_dict['pca_dim'],fit=False,fitted_model=pca_model)
-	return model.score(pca_X_test,new_y)
+	pca_X_test = do_pca(sampled_X,num_components=p_dict['pca_dim'],fit=False,fitted_model=pca_model)
+	return model.score(pca_X_test,sampled_Y)
 
 
+def scoring_function(p_dict):
+	# TODO only using train now
+
+	# Train model
+	pca_model,pca_X_train = do_pca(X_train,num_components=p_dict['pca_dim'],fit = True)
+	forest_model = random_forest(pca_X_train,y_train,num_estimators=p_dict['number_estimators'])
+
+	return [blb_main(X_test,y_test,calculate_accuracy,p_dict,pca_model,forest_model)]
 
 # def scoring_function(p_dict):
-# 	# NOTE: this does not use BLB
+# 	# NOTE: old version. this does not use BLB
 
 # 	# parameters: pca_dim,number_estimators
 
@@ -105,22 +116,6 @@ def calculate_accuracy(X,y,sample,indices,p_dict,pca_model,model):
 # 	# Test model
 # 	pca_X_test = do_pca(X_test,num_components=p_dict['pca_dim'],fit=False,fitted_model=pca_model)
 # 	return [model.score(pca_X_test,y_test)]
-
-def scoring_function(p_dict):
-	# TODO only using train now
-	# TODO clean all of this up because messy now
-
-	# Train model
-	pca_model,pca_X_train = do_pca(X_train,num_components=p_dict['pca_dim'],fit = True)
-	model = random_forest(pca_X_train,y_train,num_estimators=p_dict['number_estimators'])
-
-	#### Define variables ######
-	n = X_test.shape[0] # Size of data
-	b = int(n**(0.6)) # subset size
-	s = 1 # Number of sampled subsets
-	r = 20 # Number of Monte Carlo iterations
-
-	return [blb_main(X_test,y_test,calculate_accuracy,n,b,s,r,p_dict,pca_model,model)]
 
 
 
